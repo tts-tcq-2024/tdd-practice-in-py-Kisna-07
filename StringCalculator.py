3794fbb279a453c652980b23e4657878111e6fcb
Krushna Borse <92684634+Kisna-07@users.noreply.github.com>
2024-07-22 12:08:47 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -3,10 +3,13 @@ import re
 def add(numbers):
     if not numbers:
         return 0
+
+    delimiter, numbers = extract_delimiter(numbers)
+    number_list = split_numbers(numbers, delimiter)
     
-    delimiter = ","
-    
-    # Support for custom delimiters of any length
+    return sum_numbers(number_list)
+
+def extract_delimiter(numbers):
     if numbers.startswith("//"):
         parts = numbers.split("\n", 1)
         delimiter_part = parts[0][2:]
@@ -16,13 +19,19 @@ def add(numbers):
             delimiter = re.escape(delimiter_part[1:-1])
         else:
             delimiter = re.escape(delimiter_part)
-    
-    numbers = re.split(f"{delimiter}|\n", numbers)
-    
+    else:
+        delimiter = ","
+        
+    return delimiter, numbers
+
+def split_numbers(numbers, delimiter):
+    return re.split(f"{delimiter}|\n", numbers)
+
+def sum_numbers(number_list):
     total = 0
     negatives = []
     
-    for number in numbers:
+    for number in number_list:
         if number:
             num = int(number)
             if num < 0:
